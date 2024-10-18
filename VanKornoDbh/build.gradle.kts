9541d49f7a5e084fc24be08ea3806576b1d4c595
VanKorno <kornovandrey@gmail.com>
2024-10-18 20:33:49 +0300

    ...

--- VanKornoDbh/build.gradle.kts
+++ VanKornoDbh/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 }
 
 
-val versionTag = "1.0.5"
+val versionTag = "1.0.6"
 //val buildDirectory = layout.buildDirectory.get()
 
 publishing {    /* to ensure that the library is published correctly */
